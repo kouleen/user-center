91a922fb02d6249493551cae0a8ece0c9ca8b0ef
zhangqing.mmqg <zhangqing.mmqg@bytedance.com>
2026-08-24 17:50:41 +0800

    增加接入mysql

--- repository/user_header_repository.go
+++ repository/user_header_repository.go
@@ -8,21 +8,21 @@ import (
 )
 
 func GetUserHeaderByUsername(ctx context.Context, username string) (resp *modle.UserHeader, err error) {
-	if err = utils.GetMysqlDDB().WithContext(ctx).Model(&modle.UserHeader{}).Where("is_delete = 0 and username = ?", username).First(&resp).Error; err != nil {
+	if err = utils.GetReadMysqlDDB().WithContext(ctx).Model(&modle.UserHeader{}).Where("is_delete = 0 and username = ?", username).First(&resp).Error; err != nil {
 		return
 	}
 	return resp, nil
 }
 
 func GetUserHeaderByPhone(ctx context.Context, phone string) (resp *modle.UserHeader, err error) {
-	if err = utils.GetMysqlDDB().WithContext(ctx).Model(&modle.UserHeader{}).Where("is_delete = 0 and phone = ?", phone).First(&resp).Error; err != nil {
+	if err = utils.GetReadMysqlDDB().WithContext(ctx).Model(&modle.UserHeader{}).Where("is_delete = 0 and phone = ?", phone).First(&resp).Error; err != nil {
 		return
 	}
 	return resp, nil
 }
 
 func CreateUserHeader(ctx context.Context, userHeader *modle.UserHeader) error {
-	if err := utils.GetMysqlDDB().WithContext(ctx).Create(userHeader).Error; err != nil {
+	if err := utils.GetWriteMysqlDDB().WithContext(ctx).Create(userHeader).Error; err != nil {
 		return err
 	}
 	return nil
--- utils/mysql_client.go
+++ utils/mysql_client.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync/atomic"
 	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-var mysqlDB *gorm.DB
+var (
+	mysqlWriteDB   *gorm.DB
+	mysqlReadDB1   *gorm.DB
+	mysqlReadDB2   *gorm.DB
+	readRoundRobin uint64
+)
 
 func init() {
 	if os.Getenv("MYSQL_USERNAME") == "" {
@@ -28,12 +34,41 @@ func init() {
 	if os.Getenv("MYSQL_DATABASE") == "" {
 		log.Fatal("MYSQL_DATABASE env variable not set")
 	}
-	if err := InitMysql(os.Getenv("MYSQL_USERNAME"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_PORT"), os.Getenv("MYSQL_DATABASE")); err != nil {
+	if err := initWriteMysql(os.Getenv("MYSQL_USERNAME"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_PORT"), os.Getenv("MYSQL_DATABASE")); err != nil {
 		log.Fatal(err)
+	}
+
+	if os.Getenv("MYSQL_SLAVE1_USERNAME") != "" {
+		if os.Getenv("MYSQL_SLAVE1_PASSWORD") == "" {
+			log.Fatal("MYSQL_PASSWORD env variable not set")
+		}
+		if os.Getenv("MYSQL_SLAVE1_HOST") == "" {
+			log.Fatal("MYSQL_HOST env variable not set")
+		}
+		if os.Getenv("MYSQL_SLAVE1_PORT") == "" {
+			log.Fatal("MYSQL_PORT env variable not set")
+		}
+		if err := initRead1Mysql(os.Getenv("MYSQL_SLAVE1_USERNAME"), os.Getenv("MYSQL_SLAVE1_PASSWORD"), os.Getenv("MYSQL_SLAVE1_HOST"), os.Getenv("MYSQL_SLAVE1_PORT"), os.Getenv("MYSQL_DATABASE")); err != nil {
+			log.Fatal(err)
+		}
+	}
+	if os.Getenv("MYSQL_SLAVE2_USERNAME") != "" {
+		if os.Getenv("MYSQL_SLAVE2_PASSWORD") == "" {
+			log.Fatal("MYSQL_PASSWORD env variable not set")
+		}
+		if os.Getenv("MYSQL_SLAVE2_HOST") == "" {
+			log.Fatal("MYSQL_HOST env variable not set")
+		}
+		if os.Getenv("MYSQL_SLAVE2_PORT") == "" {
+			log.Fatal("MYSQL_PORT env variable not set")
+		}
+		if err := initRead2Mysql(os.Getenv("MYSQL_SLAVE2_USERNAME"), os.Getenv("MYSQL_SLAVE2_PASSWORD"), os.Getenv("MYSQL_SLAVE2_HOST"), os.Getenv("MYSQL_SLAVE2_PORT"), os.Getenv("MYSQL_DATABASE")); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
-func InitMysql(username, password, host, port, database string) error {
+func initWriteMysql(username, password, host, port, database string) error {
 	dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
 	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
 	if err != nil {
@@ -47,10 +82,61 @@ func InitMysql(username, password, host, port, database string) error {
 	sqlDB.SetMaxOpenConns(100)
 	sqlDB.SetConnMaxLifetime(30 * time.Second)
 	sqlDB.SetConnMaxIdleTime(30 * time.Second)
-	mysqlDB = db
+	mysqlWriteDB = db
 	return nil
 }
 
-func GetMysqlDDB() *gorm.DB {
-	return mysqlDB
+func initRead1Mysql(username, password, host, port, database string) error {
+	dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
+	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		return err
+	}
+	sqlDB.SetMaxIdleConns(20)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(30 * time.Second)
+	sqlDB.SetConnMaxIdleTime(30 * time.Second)
+	mysqlReadDB1 = db
+	return nil
+}
+
+func initRead2Mysql(username, password, host, port, database string) error {
+	dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
+	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		return err
+	}
+	sqlDB.SetMaxIdleConns(20)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(30 * time.Second)
+	sqlDB.SetConnMaxIdleTime(30 * time.Second)
+	mysqlReadDB2 = db
+	return nil
+}
+
+func GetWriteMysqlDDB() *gorm.DB {
+	return mysqlWriteDB
+}
+
+func GetReadMysqlDDB() *gorm.DB {
+	// 原子自增
+	idx := atomic.AddUint64(&readRoundRobin, 1)
+	// 取模轮询 0,1,0,1...
+	switch idx % 2 {
+	case 0:
+		return mysqlReadDB1
+	case 1:
+		return mysqlReadDB2
+	default:
+		// 兜底返回第一个
+		return mysqlReadDB1
+	}
 }
